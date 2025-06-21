e302a4523cd0df0389dcdd2256801ecfd29fd46a
manish <ms5978307@gmail.com>
2025-06-21 14:58:39 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+
+class SignLanguageCNN(nn.Module):
+    def __init__(self, num_classes: int = 29): # 26 letters + space/delete/nothing
+        super(SignLanguageCNN, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 32, 3, padding = 1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding = 1)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding = 1)
+        self.fc1 = nn.Linear(128 * 8 * 8, 512)
+        self.fc2 = nn.Linear(512, num_classes)
+    
+    def forward(self, x: Tensor):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+
+        x = x.view(-1, 128 * 8 * 8)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        
+        return x
