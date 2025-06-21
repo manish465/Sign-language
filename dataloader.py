02b938316923a65c931f8f5eb9e4e3410dd89d79
manish <ms5978307@gmail.com>
2025-06-21 17:51:05 +0530

    Create dataloader.py

---
+++ dataloader.py
@@ -0,0 +1,17 @@
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+def get_dataloaders(data_dir, batch_size = 64):
+    transform = transforms.Compose([
+        transforms.Resize((64, 64)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5] * 3, [0.5] * 3)
+    ])
+
+    train_data = datasets.ImageFolder(root="data/train", transform=transform)
+    test_data = datasets.ImageFolder(root="data/test", transform=transform)
+
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader, train_data.classes
