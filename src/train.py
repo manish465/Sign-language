c55e2e65034da0566a126cfaeb391b66f711200a
manish <ms5978307@gmail.com>
2025-06-22 11:10:22 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -10,7 +10,7 @@ model = SignLanguageCNN().to(device=device)
 
 print(f"{device.type} is in use")
 
-train_loader, test_loader, classes = get_dataloaders("data")
+train_loader, test_loader, classes = get_dataloaders("../data")
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
@@ -32,4 +32,4 @@ for epoch in range(epochs):
         running_loss += loss.item()
 
     print(f"Epoch {epoch + 1} - Loss {running_loss:.4f}")
-    torch.save(model.state_dict(), f"model_epoch_{epoch+1}.pth")
+    torch.save(model.state_dict(), f"../models/model_epoch_{epoch+1}.pth")
