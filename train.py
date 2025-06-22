bef75f8b4908c6b4d6c6fff9a523cc5247628870
manish <ms5978307@gmail.com>
2025-06-22 10:48:41 +0530

    git changes

--- train.py
+++ train.py
@@ -8,7 +8,9 @@ from dataloader import get_dataloaders
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = SignLanguageCNN().to(device=device)
 
-train_loader, val_loader, classes = get_dataloaders("data")
+print(f"{device.type} is in use")
+
+train_loader, test_loader, classes = get_dataloaders("data")
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
@@ -23,7 +25,7 @@ for epoch in range(epochs):
         images, labels = images.to(device), labels.to(device)
 
         optimizer.zero_grad()
-        outputs = model(images)
+        outputs = model.forward(images)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
